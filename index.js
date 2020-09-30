d980fce2d4f1831118dfa068e26ea71c52b39b65
Levin Weinstein <weinsteinlevin@gmail.com>
2020-09-30 16:37:09 -0700

    Adding names

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const { fdatasync } = require('fs')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+
+app.get("/", (req, res) => {
+  fdatasync.readdir(testFolder, (err, files) => {
+    res.send(files);
+  })
+})
+
+app.listen(PORT)
